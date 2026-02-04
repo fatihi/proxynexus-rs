98e7d1b54da511210457400954e4615c15ada629
Alex McCulloch <alexmccx@gmail.com>
2026-02-03 21:22:23 -0500

    even simpler arg parsing

--- proxynexus-cli/src/main.rs
+++ proxynexus-cli/src/main.rs
@@ -5,22 +5,19 @@ use clap::Parser;
 #[command(version, about, long_about = None)]
 struct Args {
     #[arg(short, long)]
-    image_path: String,
+    image_path: PathBuf,
 
-    #[arg(short, long, default_value_t = String::from("output.pdf"))]
-    output_path: String,
+    #[arg(short, long, default_value = "output.pdf")]
+    output_path: PathBuf,
 }
 
 fn main() {
     let args = Args::parse();
 
-    let image_path = PathBuf::from(&args.image_path);
-    let output_path = PathBuf::from(&args.output_path);
+    println!("Adding {:?} to {:?}!", args.image_path, args.output_path);
 
-    println!("Adding {} to {}!", args.image_path, args.output_path);
-
-    match proxynexus_core::create_pdf_with_image(&image_path, &output_path) {
-        Ok(_) => println!("PDF created successfully: {}", &args.output_path),
+    match proxynexus_core::create_pdf_with_image(&args.image_path, &args.output_path) {
+        Ok(_) => println!("PDF created successfully: {:?}", &args.output_path),
         Err(e) => eprintln!("Error: {}", e),
     }
 }
